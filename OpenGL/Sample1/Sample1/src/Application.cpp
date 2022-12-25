9403e4780c47d665e01150b89b7f2a001054e6b9
Aalokisgood <ihaveaquestion97983@gmail.com>
2022-12-25 15:02:05 +0530

    new commit

---
+++ OpenGL/Sample1/Sample1/src/Application.cpp
@@ -0,0 +1,61 @@
+/*Shaders are programs that run on GPU.
+Made for x86 architecture
+Configurations:
+    Linker
+        Input
+            Additional Dependencies
+                glfw3.lib;opengl32.lib;User32.lib;Gdi32.lib;Shell32.lib;
+        
+        General
+            Additional Library Directories:
+                $(SolutionDir)Dependencies\GLFW\lib-vc2019
+
+    C/C++
+        General
+            $(SolutionDir)Dependencies\GLFW\include
+            
+*/
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f,0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
