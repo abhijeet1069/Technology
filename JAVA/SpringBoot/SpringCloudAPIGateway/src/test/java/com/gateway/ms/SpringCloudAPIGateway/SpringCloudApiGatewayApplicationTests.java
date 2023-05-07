1ad47ff975210bc21dd87e16c95d88eb807dae31
abhijeet1069 <abhijeet1069@gmail.com>
2023-05-07 21:02:01 +0530

    new commit

---
+++ JAVA/SpringBoot/SpringCloudAPIGateway/src/test/java/com/gateway/ms/SpringCloudAPIGateway/SpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.ms.SpringCloudAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
