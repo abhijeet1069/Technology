cf8d3d8b4948f8d341990c5c9b99cdee417622ea
abhijeet1069 <abhijeet1069@gmail.com>
2023-05-26 21:02:00 +0530

    new commit

---
+++ JAVA/BlackBook/MySelf/src/constructor/Hello.java
@@ -0,0 +1,22 @@
+package constructor;
+
+
+//Can we define constructors before declaring class variables in JAVA?
+
+
+class Test{
+	public Test() {
+		this.x = 0;
+	}
+	
+	int x;
+}
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
