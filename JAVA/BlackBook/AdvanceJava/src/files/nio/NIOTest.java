714dd1c1aeba614543ea7559d09ad646520d15b9
abhijeet1069 <abhijeet1069@gmail.com>
2023-07-31 21:02:01 +0530

    new commit

--- JAVA/BlackBook/AdvanceJava/src/files/nio/NIOTest.java
+++ JAVA/BlackBook/AdvanceJava/src/files/nio/NIOTest.java
@@ -1,5 +1,13 @@
 package files.nio;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
 NIO - New File Input Output. Introduced in Java 1.4
 NIO has two fundamental items:
@@ -10,7 +18,22 @@ NIO has two fundamental items:
 public class NIOTest {
 	private static final int BSIZE  = 1024;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileChannel filech1 = new FileOutputStream("/Users/satyam/Personal/Technology/JAVA/BlackBook/AdvanceJava/res/file6.txt").getChannel();
+		filech1.write(ByteBuffer.wrap("Some text ".getBytes()));
+		filech1.close();
+		
+		filech1 = new RandomAccessFile("/Users/satyam/Personal/Technology/JAVA/BlackBook/AdvanceJava/res/file6.txt","rw").getChannel();
+		filech1.position(filech1.size());
+		filech1.write(ByteBuffer.wrap("Some more text".getBytes()));
+		filech1.close();
+		
+		filech1 = new FileInputStream("/Users/satyam/Personal/Technology/JAVA/BlackBook/AdvanceJava/res/file6.txt").getChannel();
+		ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+		filech1.read(buff);
+		buff.flip();
+		while(buff.hasRemaining())
+			System.out.print((char)buff.get());
 		
 	}
 }
