4309fb3a52e30217dd1d7e82544568275dbcb7b8
abhijeet1069 <abhijeet1069@gmail.com>
2023-08-24 21:02:01 +0530

    new commit

--- JAVA/Concurrency/WebApp/src/com/satyam/AddServlet.java
+++ JAVA/Concurrency/WebApp/src/com/satyam/AddServlet.java
@@ -1,5 +1,8 @@
 package com.satyam;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,12 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
 		
+		PrintWriter out = res.getWriter();
 		
+		out.println(i+"+"+j+"="+k);
 	}
 }
