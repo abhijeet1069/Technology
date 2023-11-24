31cd590aebfe2977905107ecd97693b5ac8248e1
abhijeet1069 <abhijeet1069@gmail.com>
2023-11-24 21:02:01 +0530

    new commit

---
+++ JAVA/JavaWorkspace/CallGraph/src/demo/Main.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Main {
+
+	public static void sayHello() {
+		System.out.println("Hello,World");
+	}
+	
+	public static void main(String[] args) {
+		sayHello();
+
+	}
+
+}
